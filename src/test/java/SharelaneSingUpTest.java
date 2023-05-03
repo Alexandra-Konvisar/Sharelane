0e7c1c77a6216f9156e02902ed2d5debdc8a69e4
Alexandra-Konvisar <46960334+Alexandra-Konvisar@users.noreply.github.com>
2023-05-03 20:17:29 +0400

    First sign test add

--- src/test/java/SharelaneSingUpTest.java
+++ src/test/java/SharelaneSingUpTest.java
@@ -5,18 +5,87 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SharelaneSingUpTest {
+public class SharelaneSingUpTest{
 
         WebDriver driver;
-1223
+
+        public class SharelaneSingUpTest extends BaseTest {
+
         @Test
         public void fiveDigitsZipCodeTest() {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-            driver =  new ChromeDriver();
             driver.get("https://www.sharelane.com/cgi-bin/register.py");
             driver.findElement(By.name("zip_code")).sendKeys("12345");
             driver.findElement(By.cssSelector("[value='Continue']")).click();
-            driver.
+        }
+
+        @Test
+        public void PositiveSingUpTest () {
+            driver.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=12345&first_name=&last_name=&email=&password1=&password2=");
+            driver.findElement(By.name("first_name")).sendKeys("Alexandra");
+            driver.findElement(By.name("last_name")).sendKeys("Konvisar");
+            driver.findElement(By.name("email")).sendKeys("test@sharelane.com");
+            driver.findElement(By.name("password1")).sendKeys("1234567");
+            driver.findElement(By.name("password2")).sendKeys("1234567");
+            WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+            Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
+        }
+
+        @Test
+        public void NumbersInNameFieldTest () {
+            driver.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=12345&first_name=&last_name=&email=&password1=&password2=");
+            driver.findElement(By.name("first_name")).sendKeys("12345");
+            driver.findElement(By.name("last_name")).sendKeys("12345");
+            driver.findElement(By.name("email")).sendKeys("test@sharelane.com");
+            driver.findElement(By.name("password1")).sendKeys("1234567");
+            driver.findElement(By.name("password2")).sendKeys("1234567");
+            WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+            Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
+        }
+
+        @Test
+        public void SpacesInNameFieldTest () {
+            driver.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=12345&first_name=&last_name=&email=&password1=&password2=");
+            driver.findElement(By.name("first_name")).sendKeys("   ");
+            driver.findElement(By.name("last_name")).sendKeys("   ");
+            driver.findElement(By.name("email")).sendKeys("test@sharelane.com");
+            driver.findElement(By.name("password1")).sendKeys("1234567");
+            driver.findElement(By.name("password2")).sendKeys("1234567");
+            WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+            Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
+        }
+
+        @Test
+        public void InvalidEmailTest () {
+            driver.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=12345&first_name=&last_name=&email=&password1=&password2=");
+            driver.findElement(By.name("first_name")).sendKeys("Alexandra");
+            driver.findElement(By.name("last_name")).sendKeys("Konvisar");
+            driver.findElement(By.name("email")).sendKeys("test_sharelane.com");
+            driver.findElement(By.name("password1")).sendKeys("1234567");
+            driver.findElement(By.name("password2")).sendKeys("1234567");
+            WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+            Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
+        }
+
+        @Test
+        public void NonMatchingPasswords () {
+            driver.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=12345&first_name=&last_name=&email=&password1=&password2=");
+            driver.findElement(By.name("first_name")).sendKeys("Alexandra");
+            driver.findElement(By.name("last_name")).sendKeys("Konvisar");
+            driver.findElement(By.name("email")).sendKeys("test@sharelane.com");
+            driver.findElement(By.name("password1")).sendKeys("1234567");
+            driver.findElement(By.name("password2")).sendKeys("12345678");
+            WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+            Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
+        }
+
+        @Test
+        public void EmptyPasswordFieldTest () {
+            driver.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=12345&first_name=&last_name=&email=&password1=&password2=");
+            driver.findElement(By.name("first_name")).sendKeys("Alexandra");
+            driver.findElement(By.name("last_name")).sendKeys("Konvisar");
+            driver.findElement(By.name("email")).sendKeys("test@sharelane.com");
+            driver.findElement(By.name("password1")).sendKeys("");
+            driver.findElement(By.name("password2")).sendKeys("12345678");
             WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
             Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
         }
