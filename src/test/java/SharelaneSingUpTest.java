2ed19aac3a274614cd03a0188e16bd590af3d5ec
Alexandra-Konvisar <46960334+Alexandra-Konvisar@users.noreply.github.com>
2023-05-03 19:38:39 +0400

    First sign test add

---
+++ src/test/java/SharelaneSingUpTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SharelaneSingUpTest {
+
+        WebDriver driver;
+1233
+        @Test
+        public void fiveDigitsZipCodeTest() {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+            driver =  new ChromeDriver();
+            driver.get("https://www.sharelane.com/cgi-bin/register.py");
+            driver.findElement(By.name("zip_code")).sendKeys("12345");
+            driver.findElement(By.cssSelector("[value='Continue']")).click();
+            driver.
+            WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+            Assert.assertTrue("Register button is displayed", registerButton.isDisplayed());
+        }
+    }
+}
